add42aadf055c631ad3d111fbcafd8f336eea97f
Nathan Marley <nathan.marley@gmail.com>
2020-09-02 12:47:00 -0300

    lifetime annotations

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,15 @@
-use std::cmp::PartialOrd;
-
 fn main() {
-    let nums = vec![34, 50, 25, 100, 65];
-    let result = largest(&nums);
-    println!("The largest number is {}", result);
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
 
-    let chars = vec!['y', 'm', 'c', 'a'];
-    let result = largest(&chars);
-    println!("The largest char is {}", result);
-
-    let strs = vec![String::from("hello"), String::from("world")];
-    println!("strs[0] = {}", strs[0]);
-    // let result = largest(&strs);
-    // println!("The largest str is {}", result);
+    let result = longest(string1.as_str(), string2);
+    println!("The longest string is {}", result);
 }
 
-fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
-    let mut largest = list[0];
-
-    for &item in list {
-        if item > largest {
-            largest = item;
-        }
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
     }
-
-    largest
 }

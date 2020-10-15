c68b393ab2e0a3ec6fdc6b6ba2f1b839bece3129
Nathan Marley <nathan.marley@gmail.com>
2020-10-15 12:05:56 -0300

    Add MyBox type

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
 fn main() {
-    let c = gg::Counter::new();
+    let x = 5;
+    let y = MyBox::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
 }

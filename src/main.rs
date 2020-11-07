f069d3cc17ccff7874128ce795ff77a7e1a150fe
Nathan Marley <nathan.marley@gmail.com>
2020-11-07 08:58:01 -0300

    Use move for spawned thread

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,14 @@
 use std::thread;
-use std::time::Duration;
 
 fn main() {
-    let handle = thread::spawn(|| {
-        for i in 1..10 {
-            println!("hi number {} from spawned thread!", i);
-            thread::sleep(Duration::from_millis(1));
-        }
+    let v = vec![1, 2, 3];
+
+    let handle = thread::spawn(move || {
+        println!("Here's a vector from spawned: {:?}", v);
     });
 
-    for i in 1..5 {
-        println!("hi number {} from the main thread!", i);
-        thread::sleep(Duration::from_millis(1));
-    }
+    // can't borrow moved value
+    // println!("Here's a vector from main: {:?}", v);
 
     handle.join().unwrap();
 }
